94309cae2d0dc11e3081213503b102e5cd95a016
NguyenTungDang <37210022+NguyenTungDang@users.noreply.github.com>
2019-08-17 21:23:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Khoa Công Nghệ Thông Tin - Trường Đại Học Thuỷ Lợi</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <?php include('header.html'); ?>
+    </header>
+    <nav>
+       
+    </nav>
+    <section>
+   
+    </section>
+   <footer>
+        
+    </footer> 
+</body>
+
+</html>
